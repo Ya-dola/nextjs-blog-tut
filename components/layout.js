d6e7c98bec7cff2ba1fee85c6e0e4c9aebbd2086
Yadola <Ya-dola@users.noreply.github.com>
2023-09-02 19:16:31 +0530

    Added Home Page Layout

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Your Name";
+const name = "Ya Dola";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
@@ -30,11 +30,11 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/profile.png"
               className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
+              height={160}
+              width={160}
+              alt="Ya Dola Profile Image"
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
@@ -43,11 +43,11 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="/images/profile.png"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
-                alt=""
+                alt="Ya Dola Profile Image"
               />
             </Link>
             <h2 className={utilStyles.headingLg}>
